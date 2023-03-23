464a7db23f8c93050fc6e45456731a41aa0ca5b5
sebbe <sebbe@DESKTOP-DHCGTGB>
2023-03-23 13:21:16 +0100

    Add project files.

---
+++ Controllers/WordfrequencyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+
+namespace WordFrequency.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class WordfrequencyController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<JsonResult> Post()
+        {
+            string text;
+            using (StreamReader reader = new StreamReader(Request.Body, Encoding.UTF8))
+            {
+                text = await reader.ReadToEndAsync();
+            }
+            WordFrequency wf = new WordFrequency();
+
+            Console.WriteLine(text);
+
+            Dictionary<string, int> dictionary = wf.CalculateWords(text);
+
+            return new JsonResult(dictionary);
+        }
+    }
+}
---
+++ WordFrequency.cs
@@ -0,0 +1,49 @@
+﻿namespace WordFrequency
+{
+    public class WordFrequency
+    {
+        /// <summary>
+        /// Calculates the amount of times a word occured in a text
+        /// </summary>
+        public Dictionary<string, int> CalculateWords(string text)
+        {        
+            char[] delimiter = { ' ', '.', ',', ';', ':', '?', '\n', '\r', '\t', '"', '(', ')', '-' };
+            string[] words = text.Split(delimiter);
+            Dictionary<string, int> wordCounts = new Dictionary<string, int>();
+
+            foreach (string word in words)
+            {
+                //Converts all the words to lowercase
+                string lowerCaseWord = word.ToLower();
+
+                if (word.Length >= 2)
+                {
+                    if (wordCounts.ContainsKey(lowerCaseWord))
+                    {
+                        wordCounts[lowerCaseWord]++;
+                    }
+                    else if (!wordCounts.ContainsKey(lowerCaseWord))
+                    {
+                        wordCounts.Add(lowerCaseWord, 1);
+                    }
+                }
+            }
+
+            //Sort by word occurence
+            Dictionary<string, int> ordered = wordCounts.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            Dictionary<string, int> mostFrequentWords = new Dictionary<string, int>();
+
+            //Trim to top 10 most occured words
+            int count = 0;
+            foreach (KeyValuePair<string, int> word in ordered)
+            {
+                if (count == 10)
+                    break;
+                mostFrequentWords.Add(word.Key, word.Value);
+                count++;
+            }
+
+            return mostFrequentWords;
+        }
+    }
+}
